d2a8ee7bf5b1abf20f2bc356c84bcce759cd4b46
Pierre Robert-Michon <pierrerm38@gmail.com>
2020-12-17 18:16:00 -0500

    main layout, about and contact sections

---
+++ script.js
@@ -0,0 +1,28 @@
+window.onscroll = function() {
+    toggleStickyNavBar()
+};
+
+// Get the navbar
+var navbar = document.getElementById("top-bar");
+
+// Get the offset position of the navbar
+var sticky = navbar.offsetTop;
+
+// Add the sticky class to the navbar when you reach its scroll position. Remove "sticky" when you leave the scroll position
+function toggleStickyNavBar() {
+  if (window.pageYOffset >= 0) {
+    navbar.classList.add("sticky")
+  } else {
+    navbar.classList.remove("sticky");
+  }
+}
+
+// Make the menu visible when you press the icon. Hide it when you click again
+function toggleMenu() {
+  var menu = document.getElementById("sections-tab");
+  if (menu.style.display === "flex") {
+    menu.style.display = "none";
+  } else {
+    menu.style.display = "flex";
+  }
+}
